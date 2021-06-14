3e24ae9e161e3db1345569c15eb1c6d51b63d240
Rahmon Muhi <ra.techno.server@protonmail.com>
2021-06-15 03:07:36 +0800

    util : Price Format update

--- src/util/priceFormat.js
+++ src/util/priceFormat.js
@@ -1,7 +1,18 @@
  export const getPriceFormat = (price) => {
+    console.log(`price in : ${price}`);
     let priceFormat = Math.floor(price / 1000)
-    priceFormat += ' ' + (price % 1000)
+    let mode = price%1000 
+    // if the mode less than 3 digits 
+    if (mode.toString().length===2){
+        mode ='0'+mode;
+    }
+     // if the mode less than 2 digits 
+    if (mode.toString().length===1){
+      mode ='00'+mode;
+    }
 
+    priceFormat += ' ' + mode
+  console.log(`price out: ${priceFormat}`);
     return priceFormat
   }
 
