837be0371a127189226ecd1f74a34204ea019b18
shahin1290 <s.patowary@yahoo.com>
2021-08-13 09:43:39 +0200

    adds styling to admin sidebar

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -13,16 +13,15 @@ import UserListScreen from '../screens/AdminScreen/UserList'
 import ReportList from '../screens/InstructorScreen/ReportList'
 
 const StyledMenu = styled.nav`
-  display: flex;
-  flex-direction: column;
-  background: #effffa;
+  background: #34495e;
   transform: ${({ open }) => (open ? 'translateX(0)' : 'translateX(-100%)')};
-  height: 100vh;
+  height: 100%;
   text-align: left;
   padding: 2rem;
-  position: absolute;
+  position: fixed;
   top: 0;
   left: 0;
+  z-index: 5;
   transition: transform 0.3s ease-in-out;
 
   @media (max-width: 576px) {
@@ -30,50 +29,71 @@ const StyledMenu = styled.nav`
   }
 `
 
-const Menu = ({ open, setSelected }) => {
+const Menu = ({ open, setBarSelected, barSelected }) => {
   const { toggleSideBar } = useSideBar()
 
   return (
     <StyledMenu open={open}>
-      <div className="d-flex">
-        <div className="sub-title mr-5">Admin Manage</div>{' '}
-        <Button variant="danger" onClick={() => toggleSideBar()}>
-          <span className="title">&times;</span>
-        </Button>{' '}
-      </div>
+      <div className="sidebar-nav">
+        <div className="d-flex ">
+          <div className="title mr-5 sidebar-brand text-white pt-2">Manage</div>{' '}
+          <Button variant="danger" onClick={() => toggleSideBar()}>
+            <span className="title">&times;</span>
+          </Button>{' '}
+        </div>
 
-      <hr />
-      <a onClick={() => setSelected('course')}>Manage Courses</a>
-      <a onClick={() => setSelected('content')}>Manage Course Content</a>
-      <a onClick={() => setSelected('media')}>Manage Media Centers</a>
-      <a onClick={() => setSelected('service')}>Manage Services</a>
-      <a onClick={() => setSelected('request')}>Request List</a>
-      <a onClick={() => setSelected('order')}>Order List</a>
-      <a onClick={() => setSelected('job')}>Job List</a>
-      <a onClick={() => setSelected('user')}>User List</a>
-      <a onClick={() => setSelected('report')}>Report List</a>
+        <hr />
+        <li>
+          {' '}
+          <a onClick={() => setBarSelected('course')} style={{background: barSelected === 'course' ? 'red' : ''}}>Manage Courses</a>
+        </li>
+        <li>
+          <a onClick={() => setBarSelected('content')}>Manage Course Content</a>
+        </li>
+        <li>
+          <a onClick={() => setBarSelected('media')}>Manage Media Centers</a>
+        </li>
+        <li>
+          <a onClick={() => setBarSelected('service')}>Manage Services</a>
+        </li>
+        <li>
+          <a onClick={() => setBarSelected('request')}>Request List</a>
+        </li>
+        <li>
+          <a onClick={() => setBarSelected('order')}>Order List</a>
+        </li>
+        <li>
+          <a onClick={() => setBarSelected('job')}>Job List</a>
+        </li>
+        <li>
+          <a onClick={() => setBarSelected('user')}>User List</a>
+        </li>
+        <li>
+          <a onClick={() => setBarSelected('report')}>Report List</a>
+        </li>
+      </div>
     </StyledMenu>
   )
 }
 
 const Sidebar = () => {
   const { sideBarOpen } = useSideBar()
-  const [selected, setSelected] = useState('course')
+  const [barSelected, setBarSelected] = useState('course')
 
   return (
     <div>
-      {selected === 'course' && <MangeCoures />}
-      {selected === 'content' && <MentorCoursesList />}
-      {selected === 'media' && <ManageMediaCenters />}
-      {selected === 'request' && <RequestList />}
-      {selected === 'job' && <JobList />}
-      {selected === 'order' && <MangeOrder />}
-      {selected === 'service' && <ManageServices />}
-      {selected === 'user' && <UserListScreen />}
-      {selected === 'report' && <ReportList />}
+      {barSelected === 'course' && <MangeCoures />}
+      {barSelected === 'content' && <MentorCoursesList />}
+      {barSelected === 'media' && <ManageMediaCenters />}
+      {barSelected === 'request' && <RequestList />}
+      {barSelected === 'job' && <JobList />}
+      {barSelected === 'order' && <MangeOrder />}
+      {barSelected === 'service' && <ManageServices />}
+      {barSelected === 'user' && <UserListScreen />}
+      {barSelected === 'report' && <ReportList />}
 
       <div>
-        <Menu open={sideBarOpen} setSelected={setSelected} />
+        <Menu open={sideBarOpen} setBarSelected={setBarSelected} />
       </div>
     </div>
   )
