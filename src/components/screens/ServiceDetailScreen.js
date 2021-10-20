addf6d8bbdb66722102da1d8d1a10610680f0e4e
Rahmon Muhi <ra.techno.server@protonmail.com>
2021-10-20 18:52:59 +0800

    deployment links

--- src/components/screens/ServiceDetailScreen.js
+++ src/components/screens/ServiceDetailScreen.js
@@ -296,7 +296,7 @@ export default function ServiceDetailScreen({ match }) {
                                   <img
                                     src={
                                       instructor.avatar
-                                        ? `http://localhost:5001/uploads/Avatar/${instructor.avatar}`
+                                        ? `https://server.ccab.tech/uploads/Avatar/${instructor.avatar}`
                                         : "/images/resource/avatar.svg"
                                     }
                                     alt='avatar'
@@ -490,7 +490,7 @@ export default function ServiceDetailScreen({ match }) {
                           className='intro-video'
                           style={{
                             backgroundImage:
-                              "url(http://localhost:5001/uploads/Service/" +
+                              "url(https://server.ccab.tech/uploads/Service/" +
                               service.img_path +
                               ")",
                           }}
