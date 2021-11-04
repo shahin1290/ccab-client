3aa164c6ee456537506c13097eb4635c88eab584
shahin1290 <s.patowary@yahoo.com>
2021-11-04 10:49:42 +0100

    fixes full payment option in checkout

--- src/App.js
+++ src/App.js
@@ -90,7 +90,7 @@ function App() {
     live: false,
   }).init();
 
-  const socket = socketIOClient("https://server.ccab.tech");
+  const socket = socketIOClient("http://localhost:5001");
 
   useEffect(() => {
     if (userDetail.user_type === "StudentUser") {
